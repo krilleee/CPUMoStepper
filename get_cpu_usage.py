83e32c55c68665aa369281a23778bed8d52ab1f9
Krille med K <krillehamann@hotmail.com>
2023-07-24 23:30:29 +0200

    Add Serial to Aduino

--- get_cpu_usage.py
+++ get_cpu_usage.py
@@ -1,10 +1,14 @@
 import psutil
 import time
+import serial
 
 def get_cpu_usage():
-    return psutil.cpu_percent(interval=1)
+    return int(psutil.cpu_percent(interval=1))
+
+ardport = serial.Serial("/dev/cu.usbmodem11301", 38400)
 
 while True:
     cpu_usage = get_cpu_usage()
     print(f"CPU Usage: {cpu_usage}%")
+    ardport.write(str(cpu_usage).encode())
     time.sleep(5)
