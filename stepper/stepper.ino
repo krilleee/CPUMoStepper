f78eec617e245109e7fff4f0451ba824a909a62e
Krille med K <krillehamann@hotmail.com>
2023-07-24 23:30:57 +0200

    First test to motor

---
+++ stepper/stepper.ino
@@ -0,0 +1,33 @@
+#include <TinyStepper.h>
+
+// Define Arduino Pin Outputs
+#define IN1 8
+#define IN2 9
+#define IN3 10
+#define IN4 11
+#define HALFSTEPS 4096  // Number of half-steps for a full rotation
+
+// Init the TinyStepper Class
+TinyStepper stepper(HALFSTEPS, IN1, IN2, IN3, IN4);
+
+void setup()
+{
+  Serial.begin(38400);
+  stepper.Enable();
+  delay(1000);
+}
+
+// First CPU Test
+void loop() {
+  if (Serial.available())
+  {
+    String a = Serial.readString();
+    Serial.print("CPU Use %: ");
+    Serial.println(a);
+
+    int b = a.toInt();
+
+    stepper.Move(b, OUTPUT);
+    delay(2000);
+  }
+}
