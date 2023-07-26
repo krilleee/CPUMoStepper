3ce705ca7ae2eafd849ac25de2e088ca94b03220
Krille med K <krillehamann@hotmail.com>
2023-07-26 23:01:22 +0200

    Removed test prints, cleaned code

--- get_cpu_usage.py
+++ get_cpu_usage.py
@@ -3,7 +3,7 @@ import time
 import serial
 
 # Change this to degrees on the clock
-deg_clock = 270;
+deg_clock = 270
 
 # Com port and data rate to Arduino
 com_port = serial.Serial("/dev/cu.usbmodem11301", 38400)
@@ -17,7 +17,7 @@ def get_cpu_usage():
     return psutil.cpu_percent(interval=1)
 
 prev_cpu_value = None
-cpu_diff = 0;
+cpu_diff = 0
 
 # Starts controller, seems to skip first value otherwise
 com_port.write(str(0).encode())
@@ -29,10 +29,10 @@ while True:
     # Print the scaled CPU value (degrees on clock) and the change since the previous measurement and sends first value to Arduino
     if prev_cpu_value is not None:
         cpu_diff = scaled_cpu_value - prev_cpu_value
-        print(f"Degrees: {scaled_cpu_value}° Change: {cpu_diff}°")
+        print(f"Degrees: {scaled_cpu_value}° Change: {cpu_diff}° CPU Usage: {get_cpu_usage()}%")
     else:
         com_port.write(str(scaled_cpu_value).encode())
-        print(f"Start Degrees: {scaled_cpu_value}°")
+        print(f"Start Degrees: {scaled_cpu_value}° CPU Usage: {get_cpu_usage()}%")
     
     # Send the value to Arduino
     time.sleep(5)
